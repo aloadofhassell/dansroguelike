831824ee3b84e954a7e06a395e4c90de89b33465
Daniel Hassell <py09dh@googlemail.com>
2021-01-11 09:19:13 +0000

    updated main to use generate_dungeon

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
