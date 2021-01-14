0bcff4536d8185cfe9c6ad3492891a4c94ada8c6
Daniel Hassell <py09dh@googlemail.com>
2021-01-14 07:51:37 +0000

    added lightning scroll

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="dansroguelike",
+        title="Dan's Terrible Roguelike",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
